f6a73ac909c6ed74cab3d9367fad29f400434ded
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-11-14 16:37:41 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -15,45 +15,41 @@ function Modal({ toggleModal, largeImage }) {
     };
   }, [toggleModal]);
 
-// class Modal extends Component {
-//   static propTypes = {
-//     toggleModal: PropTypes.func.isRequired,
-//     largeImage: PropTypes.string.isRequired,
-//   };
+  // class Modal extends Component {
+  //   static propTypes = {
+  //     toggleModal: PropTypes.func.isRequired,
+  //     largeImage: PropTypes.string.isRequired,
+  //   };
 
-//   componentDidMount() {
-//     window.addEventListener('keydown', this.handleKeyDown);
-//   }
+  //   componentDidMount() {
+  //     window.addEventListener('keydown', this.handleKeyDown);
+  //   }
 
-//   componentWillUnmount() {
-//     window.removeEventListener('keydown', this.handleKeyDown);
-//   }
+  //   componentWillUnmount() {
+  //     window.removeEventListener('keydown', this.handleKeyDown);
+  //   }
 
-//   handleKeyDown = e => {
-//     e.code === 'Escape' && this.props.toggleModal();
-//   };
+  //   handleKeyDown = e => {
+  //     e.code === 'Escape' && this.props.toggleModal();
+  //   };
 
-   const handleBackdropClick = e => {
+  const handleBackdropClick = e => {
     e.target === e.currentTarget && toggleModal();
   };
 
-
-
-    return createPortal(
-      <Backdrop onClick={handleBackdropClick}>
-        <ModalWindow>
-          <ModalImage src={largeImage} alt="" />
-        </ModalWindow>
-      </Backdrop>,
-      modalRoot
-    );
-  }
-
+  return createPortal(
+    <Backdrop onClick={handleBackdropClick}>
+      <ModalWindow>
+        <ModalImage src={largeImage} alt="" />
+      </ModalWindow>
+    </Backdrop>,
+    modalRoot
+  );
+}
 
 Modal.prototypes = {
   toggleModal: PropTypes.func.isRequired,
   largeImage: PropTypes.string.isRequired,
- 
 };
 
 export default Modal;
