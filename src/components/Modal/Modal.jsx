5ef4934dfe36ca0d32ddb50a9b83201256a8bbab
Jenny85185 <67415254+Jenny85185@users.noreply.github.com>
2022-11-14 20:16:50 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
 import { Backdrop, ModalWindow, ModalImage } from './Modal.styled';
+import { useEffect } from 'react';
+
 
 const modalRoot = document.querySelector('#modal-root');
 
@@ -14,41 +15,41 @@ function Modal({ toggleModal, largeImage }) {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [toggleModal]);
+// class Modal extends Component {
+//   static propTypes = {
+//     toggleModal: PropTypes.func.isRequired,
+//     largeImage: PropTypes.string.isRequired,
+//   };
 
-  // class Modal extends Component {
-  //   static propTypes = {
-  //     toggleModal: PropTypes.func.isRequired,
-  //     largeImage: PropTypes.string.isRequired,
-  //   };
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
 
-  //   componentDidMount() {
-  //     window.addEventListener('keydown', this.handleKeyDown);
-  //   }
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
 
-  //   componentWillUnmount() {
-  //     window.removeEventListener('keydown', this.handleKeyDown);
-  //   }
-
-  //   handleKeyDown = e => {
-  //     e.code === 'Escape' && this.props.toggleModal();
-  //   };
-
+//   handleKeyDown = e => {
+//     e.code === 'Escape' && this.props.toggleModal();
+//   };
   const handleBackdropClick = e => {
     e.target === e.currentTarget && toggleModal();
   };
 
-  return createPortal(
-    <Backdrop onClick={handleBackdropClick}>
-      <ModalWindow>
-        <ModalImage src={largeImage} alt="" />
-      </ModalWindow>
-    </Backdrop>,
-    modalRoot
-  );
+    return createPortal(
+      <Backdrop onClick={handleBackdropClick}>
+        <ModalWindow>
+          <ModalImage src={largeImage} alt="" />
+        </ModalWindow>
+      </Backdrop>,
+      modalRoot
+    );
+  
 }
 
 Modal.prototypes = {
   toggleModal: PropTypes.func.isRequired,
   largeImage: PropTypes.string.isRequired,
+ 
 };
 export default Modal;
